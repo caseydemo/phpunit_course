28157c7ab5e0b25122dc6060d52ed39bfd9efe09
Casey DeMattina <cdemattina@gmail.com>
2022-07-22 07:57:33 -0400

    added simple functions file and test for add() function within that file

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,7 +1,15 @@
 <?
 use \PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
 class ExampleTest extends TestCase {
     public function testTwoPlusTwoEqualsFour() {
         $this->assertEquals(4, 2+ 2);
+    }
+
+    public function testAddFunction() {
+        require './functions.php';
+        $this->assertEquals(4, add(2, 2));
     }
 }
