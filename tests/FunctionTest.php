30d51e1f2efd5f9504f3e5a8c8360a6fb3876ce6
Casey DeMattina <cdemattina@gmail.com>
2022-07-22 21:08:12 -0400

    added new class User.php

--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -1,8 +1,12 @@
 <?
 use PHPUnit\Framework\TestCase;
 class FunctionTest extends TestCase {
-    public function testAddFunction() {
+    public function testAddFunctionReturnsCorrectSum() {
         require './functions.php';
         $this->assertEquals(4, add(2, 2));
+    }
+
+    public function testAddFunctionDoesNotReturnIncorrectSum() {
+        $this->assertNotEquals(5, 2 + 2);
     }
 }
