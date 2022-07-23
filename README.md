3c236e48fcd87a2eb6da6efb52892e833eec225a
Casey DeMattina <cdemattina@gmail.com>
2022-07-23 13:52:27 -0400

    adding configuration values to phpunit.xml and started implementing autoloading psr-4

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # phpunit_course
 https://www.udemy.com/course/php-unit-testing/  
-
 https://phpunit.readthedocs.io/en/9.5/
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -2,7 +2,7 @@
 use PHPUnit\Framework\TestCase;
 class UserTest extends TestCase {
     public function testReturnsFullName() {
-        require './User.php';
+        
         $user = new User;
         $user->first_name = 'Teresa';
         $user->surname = 'Greene';
