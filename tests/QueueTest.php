737344b3c6f51a704aab7c73272104ac7e60be90
Casey DeMattina <cdemattina@gmail.com>
2022-08-01 16:21:07 -0400

    adding tests to the QueueTest class

---
+++ tests/QueueTest.php
@@ -0,0 +1,15 @@
+    <?
+    use PHPUnit\Framework\TestCase;
+
+    class QueueTest extends TestCase {
+        public function testNewQueueIsEmpty() {
+            $queue = new Queue;
+            $this->assertEquals(0, $queue->getCount());
+        }
+
+        public function testAnItemIsAddedToTheQueue() {
+            $queue = new Queue;
+            $queue->push('green');
+            $this->assertEquals(1, $queue->getCount());
+        }
+    }
