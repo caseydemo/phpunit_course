c7a00df7290d45df3fc7580cd20dcc31e5a9b9c8
Casey DeMattina <cdemattina@gmail.com>
2022-08-08 09:47:11 -0400

    adding/updating QueueTest

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -5,10 +5,13 @@
         public function testNewQueueIsEmpty() {
             $queue = new Queue;
             $this->assertEquals(0, $queue->getCount());
+            return $queue;
         }
-
-        public function testAnItemIsAddedToTheQueue() {
-            $queue = new Queue;
+        /**
+         * @depends testNewQueueIsEmpty
+         */
+        public function testAnItemIsAddedToTheQueue(Queue $queue) {
+            
             $queue->push('green');
             $this->assertEquals(1, $queue->getCount());
         }
